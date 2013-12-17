dfd4942df79d614fdf6ff0d0fb6591a1766d61d5
JudeMitchell <judemitchell36@gmail.com>
2013-12-17 16:14:48 -0500

    main function

--- hackpad_parser.py
+++ hackpad_parser.py
@@ -1,7 +1,19 @@
 import lxml.html
 from unidecode import unidecode
 
-def overview(raw_html):
-	html = lxml.html.fromstring(raw_html)
+def overview(html):
+	'Given an lxml HTML tree, extract the overview.'
 	xpath = '//h2[position()=2]/following-sibling::p[position()=1]/text()'
 	return unidecode(html.xpath(xpath)[0].strip())
+
+def parse(filename):
+	'Parse an HTML file from Hackpad.'
+	raw_html = open(filename).read()
+	html = lxml.html.fromstring(raw_html)
+	return {
+		"overview": overview(html),
+	}
+
+if __name__ == '__main__':
+	import json
+	print json.dumps(parse('Fixtures/moda.html'))
