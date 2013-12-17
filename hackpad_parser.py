d273eaf9c456449d5a7ca19faaf31df548302bc2
JudeMitchell <judemitchell36@gmail.com>
2013-12-17 16:52:24 -0500

    all contact types

--- hackpad_parser.py
+++ hackpad_parser.py
@@ -12,18 +12,24 @@ def parse(filename):
 	html = lxml.html.fromstring(raw_html)
 	return {
 		"overview": overview(html),
+		"contacts": {
+			'ngo': list(contacts('NGO Representatives', html)),
+			'data_ambassadors': list(contacts('Data Ambassadors', html)),
+			'volunteers': list(contacts('Volunteers', html)),
+		}
 	}
 
-def ngo_contacts(html):
+def contacts(section, html):
 	contacts = '//h2[contains(text(),"Contacts")]'
-	ngo = '/following-sibling::p[b[contains(text(),"NGO Representatives")]]'
+	group = '/following-sibling::p[b[contains(text(),"' + section + '")]]'
 	people = '/following-sibling::ul[position()=1]/li/text()'
-	
-	people_text = html.xpath(contacts + ngo + people)
+
+	people_text = html.xpath(contacts + group + people)
 	keys = ['name','position','email address']
 	for t in people_text:
 		values = [part.strip() for part in t.split(',')]
 		yield dict(zip(keys,values))
+
 
 if __name__ == '__main__':
 	import json
--- test_hackpad_parser.py
+++ test_hackpad_parser.py
@@ -15,5 +15,5 @@ def test_overview():
 	n.assert_equal(observed, expected['overview'])
 
 def test_ngo_contacts():
-	observed = h.ngo_contacts(source_html)
+	observed = h.contacts("NGO Representatives", source_html)
 	n.assert_list_equal(list(observed), expected['contacts']['ngo'])
